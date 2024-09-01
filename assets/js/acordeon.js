81f17b4a0707fbac9a683186fad5510de9ceebc6
JVBL <joaovitorlaneiro@alunos.utfpr.edu.br>
2024-09-01 17:04:56 -0300

    bruh momento

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,4 +1,5 @@
 
+
 const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
 
 acordeonTriggers.forEach((trigger) => {
@@ -6,7 +7,7 @@ acordeonTriggers.forEach((trigger) => {
         const acordeon = trigger.parentElement
         const isOpen = acordeon.classList.contains('open')
 
-        if (isOpen) {
+        if (isOpen){
             acordeon.classList.remove('open')
         } else {
             acordeon.classList.add('open')
