57ec84ccd54be6f3ccd9948d0b48e48b36931406
JVBL <joaovitorlaneiro@alunos.utfpr.edu.br>
2024-09-01 17:24:23 -0300

    huh neat

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://github.com/JoaoVBLaneiro/js-developer-portfolio/blob/81f17b4a0707fbac9a683186fad5510de9ceebc6/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
